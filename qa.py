a835d7ee347e5ed2a8f059fc4d928a3afa0cfc41
Terrance <terranceliang01@gmail.com>
2014-10-08 12:34:54 -0400

    update

--- qa.py
+++ qa.py
@@ -1,5 +1,12 @@
 from flask import Flask,render_template,request
-#import bs4, requests, google
+
+import bs4, requests, google
+
+s=google.search("Who is Spiderman?",tld='com',lang='en',num=10,start=0,stop=10,pause=2.0)
+
+
+next(s)
+
 
 app = Flask(__name__)
 
