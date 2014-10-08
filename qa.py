7ded8770870913c2fa574579aceaf4c380a948b9
OokamiNya <42chrono@gmail.com>
2014-10-08 12:04:58 -0400

    flasky skeleton code

--- qa.py
+++ qa.py
@@ -1,1 +1,15 @@
-import bs4, requests, google
+from flask import Flask,render_template,request
+#import bs4, requests, google
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+#@app.route("/form",methods=['GET','POST'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
